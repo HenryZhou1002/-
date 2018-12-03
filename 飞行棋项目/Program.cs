f0c5dba76f0b23dee54f9a7789d71f40141af0bd
HenryZhou1002 <zhouhang1002@126.com>
2018-12-03 14:21:35 +0800

    初始化地图

--- 飞行棋项目/Program.cs
+++ 飞行棋项目/Program.cs
@@ -8,8 +8,12 @@ namespace 飞行棋项目
 {
     class Program
     {
+        static int[] Maps = new int[100];  //用静态字段，来使得其被所有方法访问到。
+
         static void Main(string[] args)
         {
+
+            InitialMap();
             GameShow();
         }
 
@@ -27,6 +31,43 @@ namespace 飞行棋项目
             Console.WriteLine("**************************");
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("**************************");
+
+        }
+
+        public static void InitialMap()//初始化地图
+        {
+            int[] luckyturn = { 6, 23, 40, 55, 69, 83 }; //幸运轮盘
+            for (int i = 0; i < luckyturn.Length; i++)
+            {
+                int index = luckyturn[i];
+                Maps[index] = 1;
+            }
+
+            int[] landMine = { 5, 13, 17, 33, 38, 50, 64, 80, 94 };//地雷
+            for (int i = 0; i < landMine.Length; i++)
+            {
+                Maps[landMine[i]] = 2;
+
+            }
+
+            int[] pause = { 9, 27, 60, 93 }; //暂停
+            for (int i = 0; i < pause.Length; i++)
+            {
+                Maps[pause[i]] = 3;
+            }
+
+            int[] timeTunnel = { 20, 25, 45, 63, 72, 88, 90 }; //时空隧道
+            for (int i = 0; i < timeTunnel.Length; i++)
+            {
+                Maps[timeTunnel[i]] = 4;
+            }
+            
+
+
+        }
+
+        public static void DrawMap()//画地图
+        {
 
         }
 
