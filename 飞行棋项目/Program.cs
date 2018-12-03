36b54e60d16e3555325f29baf45245bd9444625c
HenryZhou1002 <zhouhang1002@126.com>
2018-12-03 13:03:35 +0800

    画游戏头

---
+++ 飞行棋项目/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 飞行棋项目
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GameShow();
+        }
+
+        public static void GameShow()//画游戏头
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("******.Net飞行棋游戏******");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("**************************");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("**************************");
+
+        }
+
+    }
+}
